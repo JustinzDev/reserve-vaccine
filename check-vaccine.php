ea1e8985b3e692d85ac84de1a8c7da38b2efc222
BeyondDESK <tmagconpt@gmail.com>
2021-11-03 20:41:51 +0700

    ยังไม่เสร็จ

--- check-vaccine.php
+++ check-vaccine.php
@@ -1,3 +1,4 @@
+<?php include('api/setlink.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +6,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>เช็คคิววัคซีน</title>
+    <link href="<?php echo $mylocalhost; ?>assets/css/main.css?v=<?=time();?>" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
 <body>
-    เช็คคิววัคซีน
+    <div class="divA">
+        <div class="divB">
+            <h1> ระบบตรวจสอบลำดับการจองวัคซีน <br>โรงพยาบาล Hogwarts </h1>
+            <p>ระบบการตรวจสอบลำดับการจองวัคซีน ประกอบไปด้วย แบบฟอร์มการกรอกข้อมูลเพื่อยืนยันตัวตน 
+                <br>เเละใช้ในการตรวจสอบข้อมูลของท่าน เเละท่านสามารถโอนสิทธิ์ไปให้ผู้อื่นได้ เพื่อป้องกันการสวมสิทธิ์ <br>ท่านจึงต้องกรอกข้อมูลเพื่อตรวจสอบ</p>
+            <img src="">    
+        </div>
+        <div class="divC">
+            test
+        </div>
+    </div>
 </body>
 </html>
