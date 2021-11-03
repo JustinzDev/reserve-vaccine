7abd34749efbabb84895bfae3ca1b48dbb007313
JustinzDev <fullyz1532@gmail.com>
2021-11-03 15:38:52 +0700

    แก้บัค

--- check-vaccine.php
+++ check-vaccine.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>เช็คคิววัคซีน</title>
 </head>
 <body>
-    เช็ควัคซีน
+    เช็คคิววัคซีน
 </body>
 </html>
